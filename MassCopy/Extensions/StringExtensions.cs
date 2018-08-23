f9c6c38be8bb9023d6d144b78eb81a8be953f6e5
James Klein <git@jklein.dev>
2018-08-22 23:29:08 -0500

    Add StringExtensions.RemoveLast(int) method

---
+++ MassCopy/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable CheckNamespace
+
+namespace MassCopy
+{
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Returns a new string in which the last <paramref name="n"/> characters have been removed.
+		/// </summary>
+		/// <param name="str">The string from which to remove characters.</param>
+		/// <param name="n">The number of characters to remove.</param>
+		public static string RemoveLast(this string str, int n)
+		{
+			return str.Remove(str.Length - n);
+		}
+	}
+}
