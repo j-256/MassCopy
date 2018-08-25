f026cb1132fa8d980c972af36bc9b3166fddf77b
James Klein <git@jklein.dev>
2018-08-25 15:25:12 -0500

    Rename TextBoxExtensions.cs -> WinFormsExtensions.cs

---
+++ MassCopy/Extensions/WinFormsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+// ReSharper disable CheckNamespace
+
+namespace MassCopy
+{
+	public static class WinFormsExtensions
+	{
+		public static void AppendLine(this TextBox textBox, string value)
+		{
+			if (textBox.Text.Length == 0)
+				textBox.Text = value;
+			else
+				textBox.AppendText(Environment.NewLine + value);
+		}
+
+		public static void AppendLine(this RichTextBox textBox, string value)
+		{
+			if (textBox.Text.Length == 0)
+				textBox.Text = value;
+			else
+				textBox.AppendText(Environment.NewLine + value);
+		}
+	}
+}
