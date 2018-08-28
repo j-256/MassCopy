58a9ad33ad4b63b81341a742a132ea640b851a69
James Klein <git@jklein.dev>
2018-08-28 00:48:46 -0500

    Add StringFilter class to remove certain characters from strings Add CharExtensions class to test whether a character is alpha, numeric, or other

---
+++ MassCopy/Extensions/CharExtensions.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable CheckNamespace
+
+namespace MassCopy
+{
+	public static class CharExtensions
+	{
+		public static bool IsLetter(this char c)
+		{
+			return (c >= 'A' && c <= 'Z'
+			        || c >= 'a' && c <= 'z');
+		}
+
+		public static bool IsNumeral(this char c)
+		{
+			return c >= '0' && c <= '9';
+		}
+
+		public static bool IsSpecialCharacter(this char c)
+		{
+			// Not a space or a letter or a numeral
+			return !(c == ' '
+			         || IsLetter(c)
+			         || IsNumeral(c));
+		}
+	}
+}
---
+++ MassCopy/Utilities/StringFilter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+
+namespace MassCopy.Utilities
+{
+	[Flags]
+	public enum FilterOptions
+	{
+		RemoveSpaces = 1,
+		/// <summary>
+		/// Remove all characters except alphanumerics and spaces.
+		/// </summary>
+		RemoveSpecialCharacters = 2,
+		RemoveNumerals = 4,
+		RemoveLetters = 8,
+	}
+
+	public class StringFilter
+	{
+		public FilterOptions FilterOptions { get; set; }
+
+		public StringFilter(FilterOptions filterOptions)
+		{
+			FilterOptions = filterOptions;
+		}
+
+		public string ApplyFilters(string str)
+		{
+			return ApplyFilters(str, FilterOptions);
+		}
+
+		public static string ApplyFilters(string str, FilterOptions options)
+		{
+			StringBuilder sb = new StringBuilder(str.Length);
+
+			foreach (char c in str)
+			{
+				// c == Letter
+				if (c.IsLetter())
+				{
+					if (!options.HasFlag(FilterOptions.RemoveLetters))
+						sb.Append(c);
+					continue;
+				}
+				// c == Numeral
+				if (c.IsNumeral())
+				{
+					if (!options.HasFlag(FilterOptions.RemoveNumerals))
+						sb.Append(c);
+					continue;
+				}
+				// c == Space
+				if (c == ' ')
+				{
+					if (!options.HasFlag(FilterOptions.RemoveSpaces))
+						sb.Append(c);
+					continue;
+				}
+				// c == Special character
+				else
+				{
+					if (!options.HasFlag(FilterOptions.RemoveSpecialCharacters))
+						sb.Append(c);
+					continue;
+				}
+			}
+
+			return sb.ToString();
+		}
+	}
+}
